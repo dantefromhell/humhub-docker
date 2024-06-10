9613d26e2cbaf9750a40babb9d4711411cecdb96
dantefromhell <90508808+dantefromhell@users.noreply.github.com>
2024-06-10 23:28:18 +0000

    Substitute PHP env variables properly

--- nginx/docker-entrypoint.d/60-nginx-config.sh
+++ nginx/docker-entrypoint.d/60-nginx-config.sh
@@ -7,9 +7,13 @@ export NGINX_CLIENT_MAX_BODY_SIZE="${NGINX_CLIENT_MAX_BODY_SIZE:-10m}"
 export NGINX_KEEPALIVE_TIMEOUT="${NGINX_KEEPALIVE_TIMEOUT:-65}"
 
 # shellcheck disable=SC2046
+# Substitute variables in NGINX configuration
 defined_envs=$(printf "\${%s} " $(env | grep -E "^NGINX_.*" | cut -d= -f1))
 envsubst "$defined_envs" </etc/nginx/nginx.conf >/tmp/nginx.conf
-cat /tmp/nginx.conf >/etc/nginx/nginx.conf
-rm /tmp/nginx.conf
+mv -f /tmp/nginx.conf /etc/nginx/nginx.conf
+
+# Substitute variables in PHP configuration
+envsubst </etc/php/conf.d/99-custom.ini >/tmp/99-custom.ini
+mv -f /tmp/99-custom.ini /etc/php/conf.d/99-custom.ini
 
 exit 0
